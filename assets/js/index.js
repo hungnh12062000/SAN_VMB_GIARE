43707a58eac3cbd6aa72a132767cd74388407d54
haihung <hungnh12062000@gmail.com>
2022-08-22 10:36:17 +0700

    datepicker calendar

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,17 +58,62 @@ $(document).ready(function () {
      }
 
      // Show passgenger
-     $(".count-passenger").on('click', function(){
+     $(".count-passenger").on('click', function () {
           let hasShow = $(".dropdown-box-inner").hasClass('hide'); // true
-          if(hasShow){
+          if (hasShow) {
                $(".dropdown-box-inner").show();
                $("div.dropdown-box-inner").removeClass('hide');
           } else {
                $(".dropdown-box-inner").hide();
                $("div.dropdown-box-inner").addClass('hide');
-          } 
+          }
      });
-     
+
+     // Calendar
+     // https://amsul.ca/pickadate.js/date/
+
+     let from_$input = $('#input_from').pickadate({
+          formatSubmit: 'dd//mm//yyyy',
+          monthsFull: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'],
+          weekdaysShort: ['CN', 'Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7'],
+     })
+     let from_picker = from_$input.pickadate('picker')
+
+     let to_$input = $('#input_to').pickadate({
+          formatSubmit: 'dd//mm//yyyy',
+          monthsFull: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6', 'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'],
+          weekdaysShort: ['CN', 'Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7'],
+     })
+     let to_picker = to_$input.pickadate('picker')
+
+
+     // Check if there’s a “from” or “to” date to start with.
+     if (from_picker.get('value')) {
+          to_picker.set('min', from_picker.get('select'))
+     }
+     if (to_picker.get('value')) {
+          from_picker.set('max', to_picker.get('select'))
+     }
+
+     // When something is selected, update the “from” and “to” limits.
+     from_picker.on('set', function (event) {
+          if (event.select) {
+               to_picker.set('min', from_picker.get('select'))
+          }
+          else if ('clear' in event) {
+               to_picker.set('min', false)
+          }
+     })
+     to_picker.on('set', function (event) {
+          if (event.select) {
+               from_picker.set('max', to_picker.get('select'))
+          }
+          else if ('clear' in event) {
+               from_picker.set('max', false)
+          }
+     })
+
+     // 
 
 });
 
